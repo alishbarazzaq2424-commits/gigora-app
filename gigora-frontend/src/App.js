166a4c3f083d494493b52b84784772770c364768
alishbarazzaq2424-commits <alishbarazzaq2424@gmail.com>
2026-08-07 23:20:42 +0500

    Fix template string error

--- gigora-frontend/src/App.js
+++ gigora-frontend/src/App.js
@@ -64,7 +64,7 @@ function App() {
       const token = localStorage.getItem("access_token");
 
       const response = await fetch(
-        "${process.env.REACT_APP_API_URL}/api/payment/checkout",
+        `${process.env.REACT_APP_API_URL}/api/payment/checkout`,
         {
           method: "POST",
           headers: {
