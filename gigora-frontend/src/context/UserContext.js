8e0a5ca72937d85003bd31f9f5bfa54218902b54
alishbarazzaq2424-commits <alishbarazzaq2424@gmail.com>
2026-08-07 23:13:46 +0500

    Fix production URL

--- gigora-frontend/src/context/UserContext.js
+++ gigora-frontend/src/context/UserContext.js
@@ -14,7 +14,7 @@ export const UserProvider = ({ children }) => {
         const token = localStorage.getItem("access_token");
 
         const response = await fetch(
-          "http://127.0.0.1:8000/api/usage",
+          `${process.env.REACT_APP_API_URL}/api/usage`,
           {
             headers: {
               "Authorization": `Bearer ${token}`,
